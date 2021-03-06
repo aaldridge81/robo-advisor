fa6391889927344acbd34d5b92d046e8306ef275
Alyssa Aldridge <ana81@georgetown.edu>
2021-03-06 15:41:27 -0500

    prepare to write csv data

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -78,7 +78,24 @@ recent_low = min(low_prices)
 #     
 
 
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
 
+csv_headers = ["timestamp","open","high","low","close","volume"]
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+    writer.writeheader() # uses fieldnames set above
+    
+    # looping
+    writer.writerow({
+        "timestamp": "TO DO",
+        "open": "TO DO",
+        "high": "TO DO",
+        "low": "TO DO",
+        "close": "TO DO",
+        "volume": "TO DO"
+        })
+    
 
 
 print("-------------------------")
@@ -95,7 +112,7 @@ print("-------------------------")
 print("RECOMMENDATION: BUY!") # TODO using inputs created for high and low?
 print("RECOMMENDATION REASON: TODO") # TODO using inputs created for high and low?
 print("-------------------------")
-#print(f"WRITING DATA TO CSV: {csv_file_path}") 
+print(f"WRITING DATA TO CSV: {csv_file_path}") 
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
@@ -103,13 +120,7 @@ print("-------------------------")
 
 #csv_file_path = "data/prices.csv" # a relative filepath
 
-csv_file_path = "data/prices.csv" # a relative filepath
+# csv_file_path = "data/prices.csv" # a relative filepath
 
-with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
-    writer.writeheader() # uses fieldnames set above
-    writer.writerow({"city": "New York", "name": "Yankees"})
-    writer.writerow({"city": "New York", "name": "Mets"})
-    writer.writerow({"city": "Boston", "name": "Red Sox"})
-    writer.writerow({"city": "New Haven", "name": "Ravens"})
+
 
